0360629af2e48b5c4dae15449fc23e646af8877e
MagmMe <marcin@magm.me>
2020-07-31 07:46:36 +0200

    Initial commit. Creating Storyborad and outputs for Timer

--- SimplyTimer/ViewController.swift
+++ SimplyTimer/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    var timerDisplayed = 0
+    var myTimer = Timer()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func StopBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetBtn(_ sender: UIButton) {
+    }
+    
 }
 
